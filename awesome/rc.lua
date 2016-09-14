66a3be42ea409c8c3efb626c2229c32e37d8e5c5
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2016-09-14 21:49:08 +0200

    awesome fixes (2)

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -55,20 +55,21 @@ editor_cmd = terminal .. " -e " .. editor
 modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
+-- don't change this! commenting some out renders window ordering instable!!
 layouts =
 {
-    --awful.layout.suit.floating,
+    awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
-    --awful.layout.suit.spiral,
-    --awful.layout.suit.spiral.dwindle,
-    --awful.layout.suit.max,
+    awful.layout.suit.spiral,
+    awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
-    --awful.layout.suit.magnifier
+    awful.layout.suit.magnifier
 }
 -- }}}
 
