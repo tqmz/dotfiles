aef561000b9fc0b8e13b438e188d17d6c56dcfe7
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2016-10-19 14:45:36 +0200

    repren setup

---
+++ setup/repren.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# <https://github.com/jlevy/repren> #search #replace #refactoring #nice
+sudo aptitude install python-pip &&\
+sudo pip install repren &&\
+echo 'Installed. Run `repren -h` for help.'
