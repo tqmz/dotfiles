5ab45fc564dcf85c452aecd468342f6d6aed7ed8
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2016-10-25 00:11:08 +0200

    setup: diff-so* update

--- setup/diff-highlight.sh
+++ setup/diff-highlight.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-sudo aptitude install curl
+
+# Diff Highlight
+# <https://github.com/git/git>
+# <https://github.com/git/git/tree/master/contrib/diff-highlight>
+
+. `dirname $0`/../bin/setosenv.sh
+
+case "$OPERATION_SYSTEM" in
+
+    Debian)
+        sudo aptitude install curl
+        ;;
+
+    Centos)
+        sudo yum install curl.x86_64
+        ;;
+
+    *)
+        echo '$OPERATION_SYSTEM is unknown'
+        exit 1
+
+esac
+
 curl https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight > $HOME/bin/diff-highlight && chmod +x $HOME/bin/diff-highlight
--- setup/diff-so-fancy.sh
+++ setup/diff-so-fancy.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-sudo aptitude install curl
+
+# Diff so fancy
+# <https://github.com/so-fancy/diff-so-fancy>
+
+. `dirname $0`/../bin/setosenv.sh
+
+case "$OPERATION_SYSTEM" in
+
+    Debian)
+        sudo aptitude install curl
+        ;;
+
+    Centos)
+        sudo yum install curl.x86_64
+        ;;
+
+    *)
+        echo '$OPERATION_SYSTEM is unknown'
+        exit 1
+
+esac
+
 curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/diff-so-fancy > $HOME/bin/diff-so-fancy && chmod +x $HOME/bin/diff-so-fancy
 mkdir ~/bin/libexec
 curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/libexec/diff-so-fancy.pl > $HOME/bin/libexec/diff-so-fancy.pl && chmod +x $HOME/bin/libexec/diff-so-fancy.pl
