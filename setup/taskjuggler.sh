5d6a431f7f7797f3cd0082c18552cf19c418474a
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2016-12-22 14:09:24 +0100

    taskjuggler setup

---
+++ setup/taskjuggler.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Taskjuggler – Project Management beyond Gantt Chart Drawing
+# <http://taskjuggler.org/tj3/manual/Installation.html#Installation>
+
+sudo aptitude install ruby &&\
+sudo gem install taskjuggler
