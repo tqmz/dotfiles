f3302c91799791944517201af7d9bd0b2a6f023b
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2017-01-05 14:10:49 +0100

    docker update

--- setup/docker.sh
+++ setup/docker.sh
@@ -35,3 +35,10 @@ sudo docker run hello-world # verify docker is installed correctly
 COMPOSE_FILE="/usr/local/bin/docker-compose"
 sudo wget https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` -O $COMPOSE_FILE && \
 sudo chmod +x $COMPOSE_FILE
+
+#############################################################
+# Docker Group
+#############################################################
+sudo usermod -aG docker $USER
+newgrp docker
+
