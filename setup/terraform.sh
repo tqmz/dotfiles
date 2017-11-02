e7b664e040255f6adb4d2ab5523a31f2cb607f60
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2017-11-02 10:39:05 +0100

    Terraform

---
+++ setup/terraform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Terraform – Infrastructure as Code
+# <https://www.terraform.io/intro/getting-started/install.html>
+# <https://github.com/hashicorp/terraform>
+
+
+. `dirname $0`/../bin/setosenv.sh
+
+wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.10.8/terraform_0.10.8_linux_amd64.zip
+unzip -d ~/bin /tmp/terraform.zip
