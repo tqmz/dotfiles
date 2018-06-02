b02801e20af63ea5af93477de6b8d6b914ece62f
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2018-06-02 11:03:09 +0200

    zsh installer fixes

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo aptitude install zsh && \
-sudo mkdir -p /usr/local/src && \
-cd /usr/local/src/ && \
-sudo git clone https://github.com/oh-my-zsh/oh-my-zsh.git && \
-sudo chsh -s /usr/bin/zsh $USER && \
+sudo aptitude install zsh &&\
+sudo mkdir -p /usr/local/src &&\ 
+cd /usr/local/src/ &&\
+sudo git clone https://github.com/robbyrussell/oh-my-zsh.git &&\
+sudo chsh -s /usr/bin/zsh $USER &&\
 zsh
