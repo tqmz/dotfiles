e3bd5e467fb7d28340c7ea8a580d7f575644f5c3
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2018-06-04 13:43:37 +0200

    Docker for Sid/Buster

--- setup/docker.sh
+++ setup/docker.sh
@@ -7,23 +7,32 @@
 # <https://docs.docker.com/engine/installation/linux/docker-ce/debian/>
 #######################################################################
 
-sudo aptitude purge "lcx-docker*"
-sudo aptitude purge "docker.io*"
-sudo aptitude purge "docker-engine"
-sudo aptitude update
-sudo aptitude install curl apt-transport-https ca-certificates gnupg2 software-properties-common lsb-release
+#sudo aptitude purge "lcx-docker*"
+#sudo aptitude purge "docker.io*"
+#sudo aptitude purge "docker-engine"
 
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+#sudo aptitude update
+#sudo aptitude install curl apt-transport-https ca-certificates gnupg2 software-properties-common
 
-SOURCES="/etc/apt/sources.list.d/docker.list"
-sudo touch $SOURCES
-echo "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable" | sudo tee "$SOURCES"
+#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+#sudo apt-key fingerprint 0EBFCD88 | grep Docker &&\
+
+# "buster" used here instead of "$(lsb_release -cs)" as the latter only displays "sid" => 404
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   buster \
+   edge" # on Debian Sid only "edge" is available, otherwise "stable" would be default here
+
+
+
 
 sudo aptitude update
 sudo apt-cache policy docker-ce
 sudo aptitude install docker-ce
 sudo service docker start
 sudo docker run hello-world # verify docker is installed correctly
+
+
 
 #############################################################
 # Docker Compose
