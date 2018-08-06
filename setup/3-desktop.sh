7606093047f2a390a4993b80c400fe8b31ab071c
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2018-08-06 08:36:16 +0200

    setup: konsole

--- setup/3-desktop.sh
+++ setup/3-desktop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo aptitude install konsole &&\
+sudo update-alternatives --set x-terminal-emulator /usr/bin/konsole
 sudo aptitude install i3 rofi arandr
 sudo aptitude install evolution imapfilter
