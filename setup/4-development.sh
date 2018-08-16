3996fe7c797a6c1a8a0ca49266d61883059f6603
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2018-08-16 12:51:35 +0200

    pandoc, autokey

--- setup/4-development.sh
+++ setup/4-development.sh
@@ -2,9 +2,11 @@
 
 sudo aptitude install -y libxml2-utils #xmllint #xmlcatalog
 sudo aptitude install -y tidy
-sudo aptitude install php
+sudo aptitude install -y php
+sudo aptitude install -y autokey-gtk   #textbausteine
 
 cd `dirname $0`
 ./mycli.sh
 ./composer.sh
 ./pman.sh
+./pandoc.sh
---
+++ setup/pandoc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Pandoc
+sudo aptitude install -y pandoc
+
+
+# Pandoc Themes
+# <https://github.com/diversen/pandoc-uikit>
+# <https://getuikit.com/v2/docs/customizer.html> #css #customize
+
+export PANDOC_THEMES_DIR=/opt/pandoc/themes/
+sudo mkdir -p $PANDOC_THEMES_DIR
+sudo chown -R $USER:$USER $PANDOC_THEMES_DIR
+
+cd $PANDOC_THEMES_DIR
+ln -s "`dirname $0`/../pandoc/templates/default.latex" #a4 #de #pdf
+
+#git clone https://github.com/diversen/pandoc-uikit
+
+# Usage Example:
+# pandoc https://raw.githubusercontent.com/jgm/pandoc/master/MANUAL.txt -o index.html -f markdown --template standalone.html --toc --toc-depth=2
+
+# .vimrc Example:
+# :autocmd FileType markdown noremap <C-M> :w!<CR>:!pandoc -s -f markdown -t html --template $PANDOC_THEMES_DIR/pandoc-uikit/standalone.html --toc --toc-depth=3 % -o %.html<CR>
+
+# Pandoc (Extended) Markdown Syntax:
+# <https://pandoc.org/MANUAL.html#pandocs-markdown>
+
+# Pandoc Extensions
+# <http://pandoc.org/MANUAL.html#extensions> (general)
+# <https://pandoc.org/MANUAL.html#pandocs-markdown> (for Markdown)
+
