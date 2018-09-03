cd9189ac6d9322632cf78afaac955a09881348da
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2018-09-03 10:16:22 +0200

    setup dev: recode

--- setup/4-development.sh
+++ setup/4-development.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo aptitude install -y libxml2-utils #xmllint #xmlcatalog
-sudo aptitude install -y tidy
-sudo aptitude install -y php
+sudo aptitude install -y recode tidy
+sudo aptitude install -y php php-mbstring php-zip
 sudo aptitude install -y autokey-gtk   #textbausteine
 
 cd `dirname $0`
