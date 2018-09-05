8152d499e8fa365d21ba9049f7252af1607db43f
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2018-09-05 09:33:14 +0200

    setup: desktop: suckless-tools (slock)

--- setup/3-desktop.sh
+++ setup/3-desktop.sh
@@ -2,6 +2,7 @@
 
 sudo aptitude install -y konsole &&\
 sudo update-alternatives --set x-terminal-emulator /usr/bin/konsole
+sudo aptitude install -y suckless-tools # slock
 sudo aptitude install -y i3 rofi arandr
 sudo aptitude install -y evolution imapfilter
 sudo aptitude install -y alpine alpine-doc
