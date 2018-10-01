343ebf8e06bd7a77aa9a67d51cccd8afe84cdb81
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2018-10-01 15:23:49 +0200

    setup: desktop

--- setup/3-desktop.sh
+++ setup/3-desktop.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# terminal
 sudo aptitude install -y konsole &&\
 sudo update-alternatives --set x-terminal-emulator /usr/bin/konsole
+
+# window manager
 sudo aptitude install -y suckless-tools # slock
 sudo aptitude install -y i3 rofi arandr
+
+# mail
 sudo aptitude install -y evolution imapfilter
 sudo aptitude install -y alpine alpine-doc
+
+# file manager
+sudo aptitude install -y spacefm
+
+# images
+sudo aptitude install -y gthumb
+
+# pdf viewer
+sudo aptitude install -y evince
