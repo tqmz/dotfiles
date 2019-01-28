be59381696688f3ae2963c08f981001375405304
Tim Quellmalz <tim.quellmalz@inaudito.de>
2019-01-28 14:56:06 +0100

    unattended-upgrade

--- setup/1-basics.sh
+++ setup/1-basics.sh
@@ -3,6 +3,12 @@
 sudo aptitude install -y make gcc checkinstall curl wget tree locate info ncdu
 sudo aptitude install -y postfix # MTA for crontab local mails
 
+# <https://wiki.debian.org/UnattendedUpgrades>
+sudo aptitude install -y unattended-upgrades apt-listchanges
+sudo vi /etc/apt/apt.conf.d/50unattended-upgrades
+sudo vi /etc/apt/apt.conf.d/20auto-upgrades
+sudo unattended-upgrade -d
+
 cd `dirname $0`
 ./git.sh
 ./vim.sh
