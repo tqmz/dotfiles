27399fffe154f2fe12ebbbd21f6b7b7ba73d4ab4
Tim Quellmalz <tim.quellmalz@inaudito.de>
2019-03-11 09:45:44 +0100

    setup: dev: ruby, ansible

--- setup/4-development.sh
+++ setup/4-development.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sudo aptitude install -y ansible #ansible #ansible-vault
 sudo aptitude install -y colortail multitail #logs #monitoring
 sudo aptitude install -y dos2unix recode tidy #converters
 sudo aptitude install -y shellcheck #bash #validation
 sudo aptitude install -y php php-mbstring php-zip #php
+sudo aptitude install -y ruby ruby-dev default-libmysqlclient-dev && sudo gem install bundler
 sudo aptitude install -y mysql-client #mysql
 sudo aptitude install -y libxml2-utils #xml #xmllint #xmlcatalog
 sudo aptitude install -y autokey-gtk #textbausteine
