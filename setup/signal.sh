5a8a3ba2cc752367e09ce4b8ba250081986aa55c
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2019-04-02 11:51:14 +0200

    setup: signal-desktop

--- setup/signal.sh
+++ setup/signal.sh
@@ -10,3 +10,5 @@
 curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 sudo aptitude update && sudo aptitude install signal-desktop
+
+
