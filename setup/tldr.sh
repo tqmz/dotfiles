c70af4f48b3061edcccd9f3ce714e4e62cb39882
Tim Quellmalz <tim.quellmalz@inaudito.de>
2019-06-05 13:53:20 +0200

    tldr;

---
+++ setup/tldr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# tldr;
+
+#######################################################################
+# too long, didn't read – community maintained man pages 
+# <https://tldr.sh/>
+#######################################################################
+
+gem install --user-install tldrb
