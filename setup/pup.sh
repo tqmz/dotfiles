9aad768ea999ae7dc0723a36eef8a1e9091ff304
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2019-06-26 14:45:42 +0200

    setup: Slack (updateable)

--- setup/pup.sh
+++ setup/pup.sh
@@ -5,6 +5,11 @@
 # README
 # <https://raw.githubusercontent.com/ericchiang/pup/master/README.md>
 
+if hash pup 2>/dev/null; then
+    echo "pup is already installed, version: $(pup --version)"
+    exit 0
+fi
+
 cd `dirname $0` && ./go.sh
 go get github.com/ericchiang/pup
 pup --help
--- setup/slack.sh
+++ setup/slack.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+# Slack
+
 # Release Notes
 # https://slack.com/intl/de-de/release-notes/linux
 
+# dependencies
+cd `dirname $0` && ./pup.sh
+
+# find out latest version
+version=$(curl -s https://slack.com/intl/de-de/release-notes/linux | pup '#main > div > div > div > div.legal-content > article:nth-child(1) > h2 text{}' | cut -d' ' -f 2)
+echo "Slack, latest version available: $version"
+
+# check if version is installed
+if hash slack 2>/dev/null; then
+    if [ "$version" == "$(slack --version)" ]; then
+        echo Slack is already installed in version $version.
+        exit 0
+    else
+        echo Uninstalling previous version.
+        sudo aptitude remove slack-desktop
+    fi
+fi
+
+# download and install
 cd /tmp &&\
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.2.1-amd64.deb &&\
+wget -c https://downloads.slack-edge.com/linux_releases/slack-desktop-${version}-amd64.deb &&\
 sudo aptitude install -y gconf2 gconf-service libappindicator1 &&\
-sudo dpkg -i slack-desktop-3.2.1-amd64.deb
+sudo dpkg -i slack-desktop-${version}-amd64.deb
 
