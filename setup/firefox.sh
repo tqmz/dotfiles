b83fe9b23b1f3d9b27c5a1d14662f1b426e8c5e3
Tim Quellmalz <tim.quellmalz@inaudito.de>
2020-09-28 09:38:46 +0200

    setup: firefox: now killed as "MainThread" m(

--- setup/firefox.sh
+++ setup/firefox.sh
@@ -4,7 +4,11 @@
 # <https://support.mozilla.org/de/kb/firefox-unter-linux-installieren#w_ohne-paketverwaltung-installieren>
 # <https://wiki.debian.org/Firefox#Firefox_Stable.2C_Beta_and_Nightly>
 
-killall -q firefox firefox-bin firefox-esr
+# regular kill does not work anymore, see
+# <https://forums.linuxmint.com/viewtopic.php?f=47&t=326772>
+killall -q MainThread # firefox firefox-bin firefox-esr
+
+firefox -v
 sudo aptitude remove -y firefox-esr &&\
 cd /tmp &&\
 rm -f /tmp/firefox-latest.tar.bz2 &&\
@@ -15,3 +19,4 @@ sudo mv /opt/firefox /opt/firefox-previous &&\
 sudo mkdir /opt/firefox &&\
 sudo tar xjf /tmp/firefox-latest.tar.bz2 -C /opt/firefox/ &&\
 sudo ln -sf /opt/firefox/firefox/firefox /usr/local/bin/firefox
+firefox -v
