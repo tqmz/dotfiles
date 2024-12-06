86c9e9bfad743602e902dbf1d6e1fb52b971afae
Tim Heinsohn <smile@friendsoft.de>
2024-12-06 14:08:03 +0100

    wezterm: font_size down, hide_tab_bar_if_only_one_tab true

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,14 @@
-local wezterm = require 'wezterm'
-local config = {}
+-- local wezterm = require 'wezterm'
+local config = {
+    -- launch Zsh with login option for rvm
+    default_prog = { '/bin/zsh', '-l' },
 
--- launch Zsh with login option for rvm
-config.default_prog = { '/bin/zsh', '-l' }
+    -- font
+    font_size = 10.0,
+
+    -- tab bar
+    tab_bar_at_bottom = false,
+    hide_tab_bar_if_only_one_tab = true,
+}
 
 return config
